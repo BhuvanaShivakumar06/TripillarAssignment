ab45c716939db2519e6b521e0b0271413425064e
BhuvanaShivakumar06 <bhuvanas072@gmail.com>
2026-03-20 21:05:21 +0530

    Add files via upload

---
+++ Assignment4/Module8/ReverseWords.java
@@ -0,0 +1,11 @@
+package Assignment4.Module8;
+
+public class ReverseWords {
+    public static void main(String[] args){
+        String s = "Hello World";
+        String[] words = s.split(" ");
+        for (String word : words){
+            System.out.println(new StringBuilder(word).reverse() + " ");
+        }
+    }
+}
---
+++ Assignment4/Module9/PrintNTo1.java
@@ -0,0 +1,12 @@
+package Assignment4.Module9;
+
+public class PrintNTo1 {
+    static void print(int n){
+        if(n==0) return;
+        System.out.println(n + " ");
+        print(n -1);
+    }
+    public static void main (String[] args){
+        print(5);
+    }
+}
